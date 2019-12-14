4c7e93a5db5fd491f007271af7571517ed6cced4
Egor Deryabin <egor19942012@gmail.com>
2019-12-14 22:38:11 +0700

    added TableCellController protocol

---
+++ Workout Planner/ViewControllers/Workouts/Protocols/TableCellController.swift
@@ -0,0 +1,14 @@
+//
+//  TableCellController.swift
+//  Workout Planner
+//
+//  Created by Egor Deryabin on 14/12/2562 BE.
+//  Copyright © 2562 Egor Deryabin. All rights reserved.
+//
+import UIKit
+
+protocol TableCellController {
+    static func registerCell(on tableView: UITableView)
+    func cellFromTableView(_ tableView: UITableView, forIndexPath indexPath: IndexPath) -> UITableViewCell
+    func didSelectCell()
+}
