7229e09a4738c44c82cceaabe010c6cc948dab36
Egor Deryabin <egor19942012@gmail.com>
2019-12-26 12:37:40 +0700

    configured Description view cell

--- Workout Planner/Views/Workouts/Exercises/Cells/DescriptionTableViewCell.swift
+++ Workout Planner/Views/Workouts/Exercises/Cells/DescriptionTableViewCell.swift
@@ -9,16 +9,46 @@
 import UIKit
 
 class DescriptionTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Description for exercise"
+        
+        return label
+    }()
+    
+    let descriptionField: UITextField = {
+        let field = UITextField()
+        field.borderStyle = .roundedRect
+        field.placeholder = "I.g. some caveats or notes"
+        
+        return field
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupLabel()
+        setupField()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    func setupLabel() {
+        addSubview(descriptionLabel)
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15).isActive = true
+        descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15).isActive = true
+    }
+    
+    func setupField() {
+        addSubview(descriptionField)
+        descriptionField.translatesAutoresizingMaskIntoConstraints = false
+        descriptionField.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 10).isActive = true
+        descriptionField.leadingAnchor.constraint(equalTo: descriptionLabel.leadingAnchor).isActive = true
+        descriptionField.trailingAnchor.constraint(equalTo: descriptionLabel.trailingAnchor).isActive = true
+        descriptionField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+    }
 }
